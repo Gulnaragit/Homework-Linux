75ca586fb65dc78489d85bcf3aadf2da9eed08d3
gulnaragit <gulnarachyntaeva@gmail.com>
2024-09-02 04:11:17 +0000

    new file table

---
+++ table.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo 5 multiplied by 1 = $((5*1))
+echo 5 multiplied by 2 = $((5*2))
+echo 5 multiplied by 3 = $((5*3))
+echo 6 multiplied by 4 = $((6*4))
+echo 6 multiplied by 5 = $((6*5))
+echo 6 multiplied by 6 = $((6*6))
+echo 7 multiplied by 7 = $((7*7))
+echo 7 multiplied by 8 = $((7*8))
+echo 7 multiplied by 9 = $((7*9))
+echo 567 multiplied by 5 = $((657*5))

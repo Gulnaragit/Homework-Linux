8028c3b456813672415aecc31fb0ae6ab74e78db
gulnaragit <gulnarachyntaeva@gmail.com>
2024-09-03 00:12:15 +0000

    updated table file

--- table.sh
+++ table.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-echo 5 multiplied by 1 = $((5*1))
-echo 5 multiplied by 2 = $((5*2))
-echo 5 multiplied by 3 = $((5*3))
-echo 6 multiplied by 4 = $((6*4))
-echo 6 multiplied by 5 = $((6*5))
-echo 6 multiplied by 6 = $((6*6))
-echo 7 multiplied by 7 = $((7*7))
-echo 7 multiplied by 8 = $((7*8))
-echo 7 multiplied by 9 = $((7*9))
-echo 567 multiplied by 5 = $((657*5))
+for i in {1..10}; do echo "5 multiplied by $i = $((5 * i))"; done
+for i in {1..10}; do echo "6 multiplied by $i = $((6 * i))"; done
+for i in {1..10}; do echo "7 multiplied by $i = $((7 * i))"; done
+

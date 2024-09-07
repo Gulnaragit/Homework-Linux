53f83589aca7b80e0b2320127794f7fdf0a5cafd
gulnaragit <gulnarachyntaeva@gmail.com>
2024-09-07 03:43:07 +0000

    updated calculater.sh

--- Homework12/calculator.sh
+++ Homework12/calculator.sh
@@ -7,5 +7,5 @@ read -p "Enter number two: " num2
 echo "Sum of $num1 + $num2 = $((num1+num2))"
 echo "Difference of $num1 - $num2 = $((num1-num2))"
 echo "Product of $num1 * $num2 = $((num1*num2))"
-echo "Quotient of $num1 / $num2 = $((num1/num2))"
-
+sum=$(echo "scale=2; $num1 / $num2" | bc)
+echo Quotient of $num1 / $num2 = $sum
